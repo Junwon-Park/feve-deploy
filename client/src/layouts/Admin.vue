bbe515d64d2eaf0dbc8713a8e6778f39aff27905
6879656a696e <6879656a696e@gmail.com>
2022-03-25 14:10:53 +0900

    admin 페이지 생성

--- client/src/layouts/Admin.vue
+++ client/src/layouts/Admin.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <sidebar />
-    <div class="relative md:ml-64 bg-blueGray-100">
+    <div class="relative mt-48 bg-blueGray-100">
       <admin-navbar />
-      <header-stats />
       <div class="px-4 md:px-10 mx-auto w-full -m-24">
         <router-view />
         <footer-admin />
@@ -13,15 +11,11 @@
 </template>
 <script>
 import AdminNavbar from "@/components/Navbars/AdminNavbar.vue";
-import Sidebar from "@/components/Sidebar/Sidebar.vue";
-import HeaderStats from "@/components/Headers/HeaderStats.vue";
 import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
 export default {
   name: "admin-layout",
   components: {
     AdminNavbar,
-    Sidebar,
-    HeaderStats,
     FooterAdmin,
   },
 };
