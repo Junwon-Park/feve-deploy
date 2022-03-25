c9d53c6d6f4065fb7aae9723f0a92ff3dd2ea3b9
6879656a696e <6879656a696e@gmail.com>
2022-03-25 16:16:05 +0900

    axios 추가 - client

--- client/src/main.js
+++ client/src/main.js
@@ -9,7 +9,12 @@ import { loadFonts } from './plugins/webfontloader'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@/assets/styles/tailwind.css";
 
+//axios
+import axios from 'axios'
+
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios //다른 데서 사용시 this.$axios로 사용
+
 loadFonts()
 
 new Vue({
