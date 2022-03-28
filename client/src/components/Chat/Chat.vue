81ce8c845f75ccee690e3345850c061fe3c2ac99
6879656a696e <6879656a696e@gmail.com>
2022-03-28 20:33:48 +0900

    채팅 입력창 넣음

--- client/src/components/Chat/Chat.vue
+++ client/src/components/Chat/Chat.vue
@@ -34,6 +34,12 @@
                 <v-chip>
                   안녕하세요.
                 </v-chip>
+                <v-chip
+                    class="ma-2"
+                    x-small
+                >
+                  1:00 pm
+                </v-chip>
               </v-col>
               <v-col
                   cols="12"
@@ -43,6 +49,12 @@
                 <v-chip>
                   안녕하세요.
                 </v-chip>
+                <v-chip
+                    class="ma-2"
+                    x-small
+                >
+                  1:00 pm
+                </v-chip>
               </v-col>
 
 
@@ -52,6 +64,15 @@
                   md="12"
                   class="text-right"
               >
+
+                <v-chip
+                    class="ma-2"
+                    x-small
+                    color="orange"
+                    text-color="white"
+                >
+                  1:02 pm
+                </v-chip>
                 <v-chip
                     color="orange"
                     text-color="white">
@@ -64,6 +85,15 @@
                   md="12"
                   class="text-right"
               >
+
+                <v-chip
+                    class="ma-2"
+                    x-small
+                    color="orange"
+                    text-color="white"
+                >
+                  1:03 pm
+                </v-chip>
                 <v-chip
                     color="orange"
                     text-color="white">
@@ -71,14 +101,29 @@
                 </v-chip>
               </v-col>
 
+
+              <v-col
+                  cols="12"
+                  sm="12"
+                  md="12"
+              >
+                <input type="text" name="" id="" placeholder="내용을 입력해주세요." style="width: 90%; outline: none; border-color: #ccc;">
+                <v-btn
+                    small
+                    color="orange"
+                    dark
+                >
+                  전송
+                </v-btn>
+              </v-col>
             </v-row>
           </v-container>
-          <small>* 상담내용은 따로 저장되지 않으니 주의하시기 바랍니다.</small>
+          <hr>
+          <small class="mt-10">* 상담내용은 따로 저장되지 않으니 주의하시기 바랍니다.</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
-              color="blue darken-1"
               text
               @click="dialog = false"
           >
