ddedec466e5be22f5a86e9acd0533f063a34e24e
mhk205 <mhk205@naver.com>
2022-03-29 14:43:45 +0900

    고객센터 수정 작업진행중

--- client/src/views/cscenter/Cscenter.vue
+++ client/src/views/cscenter/Cscenter.vue
@@ -1,59 +1,31 @@
 <template>
+
   <div>
-     <CscenterSidebar :currentViewState="currentViewState" @onViewStateChanged="onViewStateChanged"/>
+     
     <main class="profile-page">
-      <section class="relative block h-500-px">
-        <div
-          class="absolute top-0 w-full h-full bg-center bg-cover"
-          style="
-            background-image: url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80');
-          "
-        >
-          <span
-            id="blackOverlay"
-            class="w-full h-full absolute opacity-50 bg-black"
-          ></span>
-        </div>
-        <div
-          class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-          style="transform: translateZ(0);"
-        >
-          <svg
-            class="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              class="text-blueGray-200 fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
-      </section>
-      <section class="relative py-16 bg-blueGray-200">
-        <div class="container mx-auto px-4">
+      
+        <CscenterSidebar :currentViewState="currentViewState" @onViewStateChanged="onViewStateChanged"/>
+     
+      
+     
+      <section class="relative py-20 bg-white-200">
+        <div class="container mx-auto px-10">
           <div
-            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-auto"
           >
-            <div class="px-6">
+            <div class="px-10">
               <div class="flex flex-wrap justify-center">
                 <div
-                  class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"
+                  class="w-full lg:w-3/12 px-4 lg:order-10 flex justify-center"
                 >
                 </div> 
               </div>
-              <div class="text-center mt-12">
-                <h3
-                  class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
-                >
+              <div class="text-center mt-20 ">
+                <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
                   고객센터
                 </h3>
               </div>
-              <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+              <div class="mt-10 py-5 border-t border-blueGray-200 text-center">
                 <div class="flex flex-wrap justify-center">
                    <div class="w-full mb-12 px-4">
                     <card-table />
@@ -68,14 +40,15 @@
   </div>
 </template>
 <script>
-import CardTable from "@/components/Cards/CardTable.vue";
 import CscenterSidebar from "@/components/Sidebar/CscenterSidebar.vue";
-import team2 from "@/assets/img/team-2-800x800.jpg";
+import CardTable from "@/components/Cards/CardTable.vue";
+
+
 
 export default {
   data() {
     return {
-      team2,
+     
     };
   },
   components: {
