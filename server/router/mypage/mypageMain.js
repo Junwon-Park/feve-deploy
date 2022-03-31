7d7011cc688a3b4287afc2e822df076ccd2d4f2e
mail8617 <mail8617@naver.com>
2022-03-31 10:39:34 +0900

    mypageMain - userInfo, buy, sell 정보 db에서 가져와서 뿌려주게 추가.

--- server/router/mypage/mypageMain.js
+++ server/router/mypage/mypageMain.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { getSimpleUserInfo } = require('../../controller/mypage/mypageMain');
+const { getBuyCounts } = require('../../controller/mypage/mypageMain');
+const { getSellCounts } = require('../../controller/mypage/mypageMain');
 
 const router = express.Router();
 
 router.post('/getSimpleUserInfo', getSimpleUserInfo);
+router.post('/getBuyCounts', getBuyCounts);
+router.post('/getSellCounts', getSellCounts);
 
 module.exports = router;
