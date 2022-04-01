42977663daa73cb0989f93f5b59af77f63b7aa79
YeonJe0719 <yeonje13@naver.com>
2022-04-01 13:34:20 +0900

    chart 추가

--- client/src/views/shop/Shopview.vue
+++ client/src/views/shop/Shopview.vue
@@ -17,7 +17,7 @@
                     <p class="text-sm mb-1">PRODUCT_DESC</p>
                   </div>
                   <hr/>
-                  <div class="flex mt-3">
+                  <div class="flex mt-3 pt-3">
                     <div style="width:50%; float:left">
                     <span class="text-left" style="width:50% float:left">최근거래가</span>
                     </div>
@@ -28,19 +28,18 @@
                   
                   <div>
                     <div class="mt-3">
-                      <v-btn x-large color="error" dark style="width:48%" class="mr-2 mt-3" > 
+                      <v-btn x-large color="error" dark style="width:49%" class="mr-1 mt-3" > 
                         <div class="text-lg ">구매</div> 
                         <div class="text-lg ">10000원</div>
                       </v-btn>
-                  
-                      <v-btn x-large color="success" dark  style="width:50%" class="mt-3"> 
+                      <v-btn x-large color="success" dark  style="width:49%; float:right" class="ml-1 mt-3"> 
                         <div>판매</div> 
                         <div>20000원</div>
                       </v-btn>
                     </div>
                     
                     <div>
-                      <v-btn x-large style="width:100%" class="mt-3" > 
+                      <v-btn x-large style="width:100%; background-color:transparent" class="mt-3" > 
                           <v-btn icon color="pink">
                           <v-icon>mdi-heart</v-icon>
                           </v-btn>
@@ -50,13 +49,47 @@
                   </div>
                 </div>
                 
-                <div class="w-full mt-3">
-                    상품정보
+                <div class="w-full flex-wrap mt-3 pt-6">
+                  <h2>상품정보</h2>
+                  <div>
+                    <div style="width:33%; float:left; padding-right:10px;">
+                      <p>모델명</p>
+                      <p>PRODUCT_MNUM</p>
+                    </div>
+                    <div style="width:33%; float:left; padding-right:10px;">
+                      <p>모델명</p>
+                      <p>PRODUCT_LDATE</p>
+                    </div>
+                    <div style="width:33%; float:right; ">
+                      <p>모델명</p>
+                      <p>PRODUCT_ORIPRICE</p>
+                    </div>
+                  </div>
                 </div>
-                <hr/>
-                <div class="w-full mt-3">
-                    배송정보
+
+                <div class="w-full flex-wrap mt-3 pt-6">
+                  <h2>배송정보</h2>
+                  <div class="mt-2">
+                    <div style="float:left;">
+                      <img :src="box" alt="..." class="w-16 h-16 rounded-full border-2 border-blueGray-50 shadow"/>
+                    </div>
+                    <div>
+                      <p class="mb-2">일반배송 3000원</p>
+                      <p class="mb-2"> 검수 후 배송 ・ 5-7일 내 도착 예정</p>
+                    </div>
+                  </div>
                 </div>
+
+                <div class="w-full flex-wrap mt-3 pt-6">
+                  <h2>시세</h2>
+                  <div class="mt-2">
+                    <card-bar-chart/>
+                    <card-line-chart/>
+                    <card-table/>
+                  </div>
+                </div>
+
+              
                 
                 
               </div>
@@ -67,8 +100,14 @@
   </div>
 </template>
 <script>
+import box from "@/assets/img/box.png";
+import CardBarChart from "@/components/Cards/CardBarChart.vue";
+import CardLineChart from "@/components/Cards/CardLineChart.vue";
+import CardTable from "@/components/Cards/CardTable.vue";
+
 export default {
     data: () => ({
+      box,
       model: 0,
       colors: [
         'primary',
@@ -92,5 +131,10 @@ export default {
           },
         ],
     }),
+    components: {
+    CardBarChart,
+    CardLineChart,
+    CardTable
+  },
   }
 </script>
