ddff86400270761c43b2e13355efef0b51a25efb
6879656a696e <6879656a696e@gmail.com>
2022-04-01 16:26:29 +0900

    메인페이지 수정..힘들어서 더 못함

---
+++ client/src/components/Cards/MainCardProduct.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card
+      class=" mt-5"
+      max-width="344"
+      outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          카테고리
+        </div>
+        <v-list-item-avatar
+            tile
+            size="300"
+            color="grey"
+        ></v-list-item-avatar>
+        <v-list-item-title class="text-h5 mb-1">
+          상품명
+        </v-list-item-title>
+        <v-list-item-subtitle>상품정보</v-list-item-subtitle>
+        <v-list-item-subtitle>금액</v-list-item-subtitle>
+      </v-list-item-content>
+
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+          outlined
+          rounded
+          text
+          color="orange"
+      >
+        보러가기
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ client/src/components/Cards/Slide.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="margin-top:100px;">
+    <v-row justify="space-around">
+      <v-icon @click="model--">
+        mdi-minus
+      </v-icon>
+      {{ model }}
+      <v-icon @click="model++">
+        mdi-plus
+      </v-icon>
+    </v-row>
+    <v-carousel cycle v-model="model" height="360px" hide-delimiters>
+      <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+          width="100%"
+      >
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+<script>
+let slide1 = require('@/assets/img/slide/main_slide1.png');
+let slide2 = require('@/assets/img/slide/main_slide2.png');
+let slide3 = require('@/assets/img/slide/main_slide3.png');
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: slide1,
+        },
+        {
+          src: slide2,
+        },
+        {
+          src: slide3,
+        },
+      ],
+      model: 0,
+    }
+  },
+}
+</script>
