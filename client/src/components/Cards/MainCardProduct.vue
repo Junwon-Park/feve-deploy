f232015a9d3da6ae7f1452b8a3d22339610d2053
6879656a696e <6879656a696e@gmail.com>
2022-04-05 10:43:50 +0900

    메인 상품 가져오기 구현중

--- client/src/components/Cards/MainCardProduct.vue
+++ client/src/components/Cards/MainCardProduct.vue
@@ -1,14 +1,14 @@
 <template>
 
   <div class="flex flex-wrap">
-    <div class="w-full lg:w-6/12 xl:w-3/12 px-4 mb-5" v-for="(item, i) in items" :key="i"  >
+    <div class="w-full md:w-6/12  lg:w-4/12 xl:w-3/12 px-4 mb-5" v-for="(item, i) in items" :key="i"  >
   <div
       class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0"
   >
-    <div class="flex-auto p-4">
+    <div class="flex-auto pt-4">
       <div class="flex flex-wrap">
         <div class="relative w-full max-w-full flex-grow flex-1">
-          <div class="w-full mb-5" style="height: 20vh; overflow: hidden;" >
+          <div class="w-full mb-5" style="overflow: hidden; width:15vw; height: 15vw;" >
             <img alt="아이템 사진" style="object-fit: cover;border-radius: .5rem;  background: #f4f4f4; width:100%; height: 100%;">
           </div>
           <h5 class="text-black uppercase font-bold text-xs underline">
@@ -36,6 +36,16 @@ export default {
   data(){
     return{
       items:[{
+        cate:"lego",
+        title:"가나다",
+        desc:"설명중ㅇㅇㅇ",
+        price: 550000,
+      },{
+        cate:"lego",
+        title:"가나다",
+        desc:"설명중ㅇㅇㅇ",
+        price: 550000,
+      },{
         cate:"lego",
         title:"가나다",
         desc:"설명중ㅇㅇㅇ",
---
+++ server/controller/main/loadproduct.js
@@ -0,0 +1,29 @@
+const { Product } = require('../../models');
+const { Category } = require('../../models');
+const db = require('../../models');
+const Sequelize = require('sequelize');
+
+async function loadproduct(req, res, next) {
+  await db.sequelize
+    .query(
+      'SELECT \n' +
+        'PRODUCT_KEY \n' +
+        ',PRODUCT_NAME \n' +
+        ',PRODUCT_MNUM \n' +
+        ',PRODUCT_ORIPRICE \n' +
+        ',PRODUCT_DELETE \n' +
+        ',c.CATEGORY_DESC  as PRODUCT_CATE \n' +
+        ',ct.CATEGORY_desc  as PRODUCT_BRAND \n' +
+        'from Product\n' +
+        'left outer join Category c on c.CATEGORY_KEY =PRODUCT_CATE \n' +
+        'left outer join Category ct on c.CATEGORY_parent=ct.CATEGORY_NAME;',
+      { type: Sequelize.QueryTypes.SELECT }
+    )
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
+}
+
+module.exports = { loadproduct };
