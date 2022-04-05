c1d64cfeca0440e3949daf648e6eff4fe1ab72c7
6879656a696e <6879656a696e@gmail.com>
2022-04-05 10:43:19 +0900

    차트 수정

--- client/src/components/Cards/Admin/AdminBarChart.vue
+++ client/src/components/Cards/Admin/AdminBarChart.vue
@@ -30,6 +30,17 @@ export default {
   },
   created: function () {
     this.$nextTick(function () {
+      this.drawChart()
+    });
+  },
+  ready() {
+    this.$nextTick(function() {
+      this.drawChart();
+    })
+  },
+  methods: {
+    drawChart(){
+
       let config = {
         type: "bar",
         data: {
@@ -133,114 +144,7 @@ export default {
       };
       let ctx = document.getElementById("bar-chart").getContext("2d");
       window.myBar = new Chart(ctx, config);
-    });
-  },
-  updated: function () {
-    this.$nextTick(function () {
-      let config = {
-        type: "bar",
-        data: {
-          labels: [
-            "1월",
-            "2월",
-            "3월",
-            "4월",
-            "5월",
-            "6월",
-            "7월",
-            "8월",
-            "9월",
-            "10월",
-            "11월",
-            "12월",
-          ],
-          datasets: [
-            {
-              //label: new Date().getFullYear(),
-              label: "판매",
-              backgroundColor: "#01856B",
-              borderColor: "#01856B",
-              //data: [30, 78, 56, 34, 100, 45, 13, 78, 56, 34, 100, 44],
-              data: this.bidBuyCnt,
-              fill: false,
-              barThickness: 8,
-            },
-            {
-              //label: new Date().getFullYear() - 1,
-              label: "판매",
-              fill: false,
-              backgroundColor: "#EF4444",
-              borderColor: "#EF4444",
-              //data: [27, 68, 86, 74, 10, 4, 87, 28, 27, 68, 86, 74, ],
-              data: this.bidSellCnt,
-              barThickness: 8,
-            },
-          ],
-        },
-        options: {
-          maintainAspectRatio: false,
-          responsive: true,
-          title: {
-            display: false,
-            text: "Orders Chart",
-          },
-          tooltips: {
-            mode: "index",
-            intersect: false,
-          },
-          hover: {
-            mode: "nearest",
-            intersect: true,
-          },
-          legend: {
-            labels: {
-              fontColor: "rgba(0,0,0,.7)",
-            },
-            align: "end",
-            position: "bottom",
-          },
-          scales: {
-            xAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: true,
-                  labelString: new Date().getFullYear()+" 월별 통계",
-                },
-                gridLines: {
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.3)",
-                  zeroLineColor: "rgba(33, 37, 41, 0.3)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-            yAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: "Value",
-                },
-                gridLines: {
-                  borderDash: [2],
-                  drawBorder: false,
-                  borderDashOffset: [2],
-                  color: "rgba(33, 37, 41, 0.2)",
-                  zeroLineColor: "rgba(33, 37, 41, 0.15)",
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-          },
-        },
-      };
-      let ctx = document.getElementById("bar-chart").getContext("2d");
-      window.myBar = new Chart(ctx, config);
-    });
-  },
+    }
+  }
 };
 </script>
