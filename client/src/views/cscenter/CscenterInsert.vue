6ea7c04f56b533e39363bf1252f36a5540dcd835
mhk205 <mhk205@naver.com>
2022-04-06 14:32:21 +0900

    cscenter insert 페이지 작성

--- client/src/views/cscenter/CscenterInsert.vue
+++ client/src/views/cscenter/CscenterInsert.vue
@@ -10,129 +10,69 @@
               >
                 <div class="flex-auto p-5 lg:p-10">
                   <h4 class="text-2xl font-semibold">
-                    상품 등록
+                    문의 등록
                   </h4>
 
                   <div class="flex flex-wrap">
                   <div class="relative mb-3 mt-8 w-full lg:w-6/12 pr-3 ">
                     <label
                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                        htmlFor="상품명"
+                        htmlFor="제목"
                     >
-                      상품명
+                      제목
                     </label>
                     <input
                         type="text"
                         class="border-0 px-3 py-3  placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                        placeholder="상품명"
-                        v-model="product.product_name"
+                        placeholder="제목"
+                        v-model="cscenter.cscenter_title"
                     />
                   </div>
 
                     <div class="relative mb-3  mt-8 w-full lg:w-6/12 pr-3 ">
                       <label
                           class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="모델명"
+                          htmlFor="작성자"
                       >
-                        모델명
+                        작성자
                       </label>
                       <input
                           type="text"
                           class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="모델명"
-                          v-model="product.product_mnum"
+                          placeholder="작성자"
+                          v-model="cscenter.user_key"
                       />
                     </div>
 
                     <div class="relative mb-3  mt-8 w-full lg:w-6/12 pr-3 ">
                       <label
                           class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="브랜드명"
+                          htmlFor="작성일"
                       >
-                        브랜드명
-                      </label>
-
-                      <select  id="brand" class=" w-full px-3 py-3 shadow"
-                               @change="secondClassCate($event)" v-model="product.product_brand">
-                        <option v-for="( brand, idx ) in product.product_get_brand" :key="idx" :value="brand['CATEGORY_NAME']">{{  brand['CATEGORY_DESC'] }}</option>
-                      </select>
-                    </div>
-
-                    <div class="relative mb-3  mt-8 w-full lg:w-6/12 pr-3 ">
-                      <label
-                          class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="카테고리"
-                      >
-                        카테고리
-                      </label>
-                      <select  id="category" class=" w-full px-3 py-3 shadow"  v-model="product.product_cate" disabled >
-                        <option  v-for="( cate, idx ) in product.product_get_cate" :key="idx" :value="cate['CATEGORY_KEY']">{{  cate['CATEGORY_DESC'] }}</option>
-                      </select>
-                    </div>
-
-                    <div class="relative mb-3  mt-8 w-full lg:w-6/12 pr-3 ">
-                      <label
-                          class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="출시일"
-                      >
-                        출시일
+                        작성일
                       </label>
                       <input
                           type="date"
                           class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="출시일"
-                          v-model="product.product_ldate"
-                      />
-                    </div>
-
-                   <div class="relative mb-3  mt-8 w-full lg:w-6/12 pr-3 ">
-                     <form>
-                      <label
-                          class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="상품사진경로"
-                      >
-                        사진
-                      </label>
-                      <input
-                          type="file"
-                          id="uploadImg"
-                          name="image"
-                          class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="사진경로"
-                          @change="handleImage"
-                      />
-                     </form>
-                  </div>
-
-                    <div class="relative mb-3  mt-8 w-full lg:w-6/12 pr-3 ">
-                      <label
-                          class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="상품원가"
-                      >
-                        상품원가(발매가)
-                      </label>
-                      <input
-                          type="text"
-                          class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="상품원가"
-                          v-model="product.product_oriprice"
+                          placeholder="작성일"
+                          v-model="cscenter.cscenter_wdate"
                       />
                     </div>
 
                   <div class="relative w-full mb-3">
                     <label
                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                        htmlFor="상품 설명"
+                        htmlFor="문의 내용"
                     >
-                      상품 설명
+                      문의 내용
                     </label>
                     <textarea
                         rows="15"
                         cols="80"
                         class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                        placeholder="상품 설명"
+                        placeholder="문의 내용"
                         style="background: #fff !important;"
-                        v-model="product.product_desc"
+                        v-model="cscenter.cscenter_contents"
                     />
                   </div>
                   <div class="text-center mt-6">
@@ -158,52 +98,21 @@
 export default {
   data() {
     return {
-      product: {
-        product_name: '',
-        product_mnum: '',
-        product_ldate: '',
-        product_pic: '',
-        product_oriprice: '',
-        product_desc: '',
-        product_wdate:'',
-        product_brand: '',
-        product_get_brand: '',
-        product_cate: '',
-        product_get_cate: '',
+      cscenter: {
+        cscenter_title: '',
+        cscenter_contents: '',
+        cscenter_wdate: '',
+        cscenter_status: '',
+        user_key: '',
         default: "0",
-      },
-      uploadServerImg:'',
+      }
     };
   },
 
   components: {
 
   },
-
-  //돔이 그려지기 시작하면 대분류 카테고리 가져오기
-  created() {
-    let that = this;
-    this.$axios.get('http://localhost:8080/category')
-        .then(function(res){
-          that.product.product_get_brand = res.data;
-        })
-        .catch(function(err){
-          console.log(err);
-        });
-  },
-
   methods: {
-    handleImage(e){
-      this.uploadServerImg = e.target.files;
-      let that = this;
-      if(this.uploadServerImg) {
-        that.product.product_pic=this.uploadServerImg[0].name;
-      }
-      else {
-        that.product.imgsrc = 'icon_question';
-      }
-    },
-
     formSubmit() {
       const curr = new Date();
       const utc =curr.getTime() + (curr.getTimezoneOffset() * 60 * 1000);
@@ -212,59 +121,20 @@ export default {
 
 
 
-      this.$axios.post('http://localhost:8080/admin/addproduct', {
-          product_brand: this.product.product_brand,
-          product_name: this.product.product_name,
-          product_mnum: this.product.product_mnum,
-          product_ldate: this.product.product_ldate,
-          product_pic: this.product.product_pic,
-          product_desc: this.product.product_desc,
-          product_oriprice: this.product.product_oriprice,
-          product_wdate: kr_curr,
-          product_cate: this.product.product_cate
+      this.$axios.post('http://localhost:8080/cscenter/cscenterinsert', {
+          cscenter_title: this.cscenter.cscenter_title,
+          cscenter_contents: this.cscenter.cscenter_contents,
+          cscenter_wdate: kr_curr,
+          cscenter_status: this.cscenter.cscenter_status,
+          user_key:this.cscenter.user_key
       })
           .then(() => {
-            alert("상품이 등록되었습니다.");
-            this.$router.push('/admin');
+            alert("문의가 등록 되었습니다.");
+            this.$router.push('/cscenter');
           })
           .catch((error) => {
             console.log(error);
-          });
-
-      // let that = this;
-      let file = this.uploadServerImg[0];
-      const formData = new FormData();
-      formData.append("image", file);
-
-      console.log(file)
-
-      this.$axios.post('http://localhost:8080/uploadImage', formData, {
-        headers: {
-          'content-type': 'multipart/form-data'
-        }
-      })
-          .then(function(res){
-            console.log(res)
-          })
-          .catch(function(err){
-            console.log(err)
-          })
-    },
-
-    //중분류 가져오기
-    secondClassCate( event ){
-      console.log(event.target.value);
-      let that = this;
-      this.$axios.post('http://localhost:8080/category/second', {
-        product_brand: event.target.value,
-      })
-          .then(function(res){
-            that.product.product_get_cate = res.data;
-            document.querySelector('#category').disabled=false;
-          })
-          .catch(function(err){
-            console.log(err);
-          })
+          }); 
     }
   }
 };
