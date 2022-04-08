bd5e1fb4eaff8e4fda8286e57bdefbc797d4d2fb
YeonJe0719 <yeonje13@naver.com>
2022-04-08 13:28:37 +0900

    카테고리 필터링 쿼리 수정

--- server/controller/shop/filter.js
+++ server/controller/shop/filter.js
@@ -28,16 +28,21 @@ async function filter(req, res, next) {
             ',a.PRODUCT_ORIPRICE \n' +
             ',a.PRODUCT_WDATE\n' +
             ',a.PRODUCT_CATE\n' +
-            ',b.SELL_PRICE \n' +
-        'FROM Product a\n'+
-        'INNER JOIN Sell b\n'+
-        'ON b.PRODUCT_KEY = a.PRODUCT_KEY \n' +
-        'AND a.PRODUCT_CATE IN (' +
-          '' +
-          cate +
-          ')' +
-          '\n' +
-          'AND b.SELL_PRICE = (SELECT MIN(SELL_PRICE) FROM Sell WHERE PRODUCT_KEY = a.PRODUCT_KEY AND SELL_STATUS=0)\n;',
+            ',(SELECT MIN(SELL_PRICE) FROM Sell WHERE PRODUCT_KEY = a.PRODUCT_KEY AND SELL_STATUS=0)AS SELL_PRICE\n' +
+        'FROM \n' +
+        '(SELECT \n' +
+            'a.PRODUCT_KEY \n' +
+            ',a.PRODUCT_BRAND \n' +
+            ',a.PRODUCT_NAME \n' +
+            ',a.PRODUCT_MNUM \n' +
+            ',a.PRODUCT_LDATE \n' +
+            ',a.PRODUCT_PIC \n' +
+            ',a.PRODUCT_DESC \n' +
+            ',a.PRODUCT_ORIPRICE \n' +
+            ',a.PRODUCT_WDATE\n' +
+            ',a.PRODUCT_CATE\n' +
+            'FROM Product a\n'+
+            'WHERE a.PRODUCT_DELETE = 0 AND a.PRODUCT_CATE IN ('+ cate + '))a;',
         { type: sequelize.QueryTypes.SELECT }
       )
       .then((result) => {
