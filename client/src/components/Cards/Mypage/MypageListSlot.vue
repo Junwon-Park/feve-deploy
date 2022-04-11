035f98aba170bba3e79d312110b4146468f27ef7
mail8617 <mail8617@naver.com>
2022-04-11 09:46:15 +0900

    mypageListSlot  - 텍스트, 버튼 간격 넣음 (지금은 일정한 간격으로 배치)

--- client/src/components/Cards/Mypage/MypageListSlot.vue
+++ client/src/components/Cards/Mypage/MypageListSlot.vue
@@ -1,72 +1,72 @@
 <template>
-          <div class="divide-y-b-gray flex"
-          style="align-items:center">
-            <img
-              :src= "imageUrl + item.Product.PRODUCT_PIC"
-              style="border-radius: 8px; width:5rem; height:5rem; margin-top:1rem; margin-bottom:1rem"
-              crossorigin
-            >
-            
-            <div class="ml-3">
-                <h5 class="font-bold underline mt-2">
-                {{ item.Product.PRODUCT_BRAND }}
-                </h5>
-                <h5>
-                {{ item.Product.PRODUCT_NAME }}
-                </h5>
-            </div>
-            &nbsp;
+  <v-row class="divide-y-b-gray"
+  style="align-items:center">
+    
+    <v-col cols="12">
+      <img
+        :src= "imageUrl + item.Product.PRODUCT_PIC"
+        style="border-radius: 8px; width:5rem; height:5rem; margin-top:1rem; margin-bottom:1rem"
+        crossorigin
+      >
+    </v-col>
+      
+    <v-col cols="12" class="ml-3" :align-self="center">
+        <h5 class="font-bold underline mt-2">
+        {{ item.Product.PRODUCT_BRAND }}
+        </h5>
+        <h5>
+        {{ item.Product.PRODUCT_NAME }}
+        </h5>
+    </v-col>
 
-            <!-- <h5 class="text-gray-400" style="font-size: 0.7rem; line-height: 0.75rem; margin-top:-0.2rem">즉시 구매가</h5> -->
+    <!-- <h5 class="text-gray-400" style="font-size: 0.7rem; line-height: 0.75rem; margin-top:-0.2rem">즉시 구매가</h5> -->
 
-            <div>
-              <h5 class="font-bold underline mt-2">
-              {{ item.strState }}
-              </h5>
-            </div>
-            &nbsp;
+    <v-col cols="12" :align-self="center">
+      <h5 class="font-bold underline mt-2">
+      {{ item.strState }}
+      </h5>
+    </v-col>
 
-            <div>
-              <h5 class="font-bold mt-1">
-                {{ price.toLocaleString('ko-KR') }}원
-              </h5>
-            </div>
-            &nbsp;
+    <v-col cols="12" :align-self="center">
+      <h5 class="font-bold mt-1">
+        {{ price.toLocaleString('ko-KR') }}원
+      </h5>
+    </v-col>
+      
+    <v-col cols="12" :align-self="center">
+      <h5>
+        {{ eDate }}
+      </h5>
+    </v-col>
+      
+    <!-- <v-list-item-action > -->
+      <v-col cols="12" :align-self="center" v-if="IsFinalize()">
+        <v-btn
+          style="background: #EF6253; color: white;"
+          rounded
+          text
+          @click="onFinalizeClicked(0)"
+        >
+          구매확정
+        </v-btn>
+      </v-col>
 
-            <div>
-              <h5>
-                {{ eDate }}
-              </h5>
-            </div>
-            &nbsp;
-            
-            <div v-if="IsFinalize()" >
-            <v-spacer></v-spacer>
-            <v-list-item-action>
-              <v-btn
-                  style="background: #EF6253; color: white;"
-                  rounded
-                  text
-                  @click="onFinalizeClicked(0)"
-              >
-                구매확정
-              </v-btn>
-
-              <v-btn
-                  style="background: #EF6253; color: white;"
-                  rounded
-                  text
-                  @click="onFinalizeClicked(1)"
-              >
-                구매취소
-              </v-btn>
-              </v-list-item-action>
-            </div>
-          </div>
+      <v-col cols="12" :align-self="center" v-if="IsFinalize()">
+        <v-btn
+          style="background: #EF6253; color: white;"
+          rounded
+          text
+          @click="onFinalizeClicked(1)"
+        >
+          구매취소
+        </v-btn>
+      </v-col>
+    <!-- </v-list-item-action> -->
+    
+  </v-row>
 </template>
 
 <script>
-
 export default {
   props:{
     slotKey:Number,
