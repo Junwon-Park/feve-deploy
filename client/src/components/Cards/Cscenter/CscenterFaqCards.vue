19d5b23949683e18df2a8eb06a1ab4952d6151ac
mhk205 <mhk205@naver.com>
2022-04-11 14:15:58 +0900

    cscenter FAQ 수정중

--- client/src/components/Cards/Cscenter/CscenterFaqCards.vue
+++ client/src/components/Cards/Cscenter/CscenterFaqCards.vue
@@ -25,41 +25,7 @@ export default {
     return {
 
     }
-  },
-  props: {
-    CSCENTER_KEY: {
-        default: 0
-      },
-    CSCENTER_TITLE: {
-        type: String,
-        default:"아이디 비번 찾기 질문 있어요~"
-      },
-    CSCENTER_CONTENTS:  {
-        type: String,
-        default:"어떤 페이지에서 찾나요?"
-      },
-    CSCENTER_WDATE:  {
-        type: String,
-        default:"2022-04-01"
-      },
-    CSCENTER_STATUS: {
-        default: ""
-      },
-    CSCENTER_COMMENT:   {
-        type: String,
-        default:"없음"
-      },
-    USER_ID:  {
-      type: String,
-      default: 'test'
-      },
-    USER_KEY:{
-      default: 0
-    },
-  },
-
-  methods: {
-    
-  },
+  }
+ 
 }
 </script>
