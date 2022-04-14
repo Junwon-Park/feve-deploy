13c7d41ef319251dc22caec31e1fd8e996f79f1a
mail8617 <mail8617@naver.com>
2022-04-14 13:15:18 +0900

    Navbar에서 관심상품 클릭했을대 마이페이지로 이동.

--- client/src/components/Navbars/IndexNavbar.vue
+++ client/src/components/Navbars/IndexNavbar.vue
@@ -88,7 +88,7 @@
             </router-link>
           </li>
           <li class="flex items-center" v-if="$store.state.isLogin">
-            <router-link to="/shop">
+            <router-link to="/mypage">
               <span
                 class="text-blueGray-800 px-3 py-2 flex items-center text-xs uppercase"
               >
