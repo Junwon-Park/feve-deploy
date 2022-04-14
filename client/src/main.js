3b6e1c4a5a2285ea345705f7c2038725b1a414e8
Junwon-Park <upjw0429@gmail.com>
2022-04-14 18:08:30 +0900

    deploy test - 6

--- client/src/main.js
+++ client/src/main.js
@@ -14,7 +14,7 @@ import axios from 'axios';
 
 // socket.io
 import io from 'socket.io-client';
-const socket = io('ec2-52-23-226-152.compute-1.amazonaws.com');
+const socket = io('http://ec2-52-23-226-152.compute-1.amazonaws.com:8080');
 
 Vue.prototype.$socket = socket;
 Vue.config.productionTip = false;
