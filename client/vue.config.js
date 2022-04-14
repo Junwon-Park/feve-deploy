99610354658be7f11b0f264b68328073730f4b55
Junwon-Park <upjw0429@gmail.com>
2022-04-14 17:21:48 +0900

    deploy test - 3

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 module.exports = {
   transpileDependencies: ['vuetify'],
-  devServer: {
-    https: {
-      key: fs.readFileSync('./certKey/key.pem'),
-      cert: fs.readFileSync('./certKey/cert.pem')
-    },
-    public: 'https://localhost:3000',
-    host: 'localhost'
-  }
+  // devServer: {
+  //   https: {
+  //     key: fs.readFileSync('./certKey/key.pem'),
+  //     cert: fs.readFileSync('./certKey/cert.pem')
+  //   },
+  //   public: 'https://localhost:3000',
+  //   host: 'localhost'
+  // }
 };
