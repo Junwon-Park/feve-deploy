6be67987e08d055dcf38de6e40efc276cf182893
Junwon-Park <upjw0429@gmail.com>
2022-04-14 18:18:25 +0900

    deploy test - 8

--- server/app.js
+++ server/app.js
@@ -159,7 +159,8 @@ if (fs.existsSync('./certKey/key.pem') && fs.existsSync('./certKey/cert.pem')) {
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://feve.link:3000',
+    origin:
+      'http://s3-project-feve.s3-website.ap-northeast-2.amazonaws.com/:3000',
     credentials: true
   },
   allowEIO3: true
