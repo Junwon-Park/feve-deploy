bb5d7dc737636f1221a7463d72c35637f597c2b0
Junwon-Park <upjw0429@gmail.com>
2022-04-28 22:07:34 +0900

    deploy test 75 - scripts/initialize.sh yarn으로 수정

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/feve-deploy/server
-npm install
-npm install pm2@latest -g
-sudo apt-get update
-sudo apt-get install authbind
-sudo service codedeploy-agent restart
-sudo service codedeploy-agent status
-sudo touch /etc/authbind/byport/80
-sudo chown ubuntu /etc/authbind/byport/80
-sudo chmod 755 /etc/authbind/byport/80
+sudo yarn
+sudo yarn global add pm2
